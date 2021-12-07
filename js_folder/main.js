76c61081d9abaeac72bafaeb366b6dc7af174f2c
Jake Sexton <drained77@gmail.com>
2021-12-07 17:36:39 -0500

    I've added JS to display the results of the damage a great sword can do. I've added 3 funtions and started actually working on the game itself. I added a hero image to both pages. I added several buttons with ID's for the game.

--- js_folder/main.js
+++ js_folder/main.js
@@ -2,5 +2,38 @@ function toggleMenu () {
     document.getElementById("primaryNav").classList.toggle("open");
 }
 
-        let x = document.getElementById("hamburgerBtn");
+let x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+function final(){
+    let d6One = rndDice(1, 7);
+    let d6Two = rndDice(1, 7);
+    let d6Three = rndDice(1, 7);
+  
+    let final = d6One + d6Two + d6Three;
+    return final;
+  }
+  
+  
+  function rndDice(min, max){
+      return Math.floor(Math.random() * (max - min)) + min;
+  
+  }
+
+  function blacksmithFunction() {
+    var x = document.getElementById("blacksmith");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function forestLoseFunction() {
+    var x = document.getElementById("forestLose");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
